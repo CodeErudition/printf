405d53d262b6907eb8f418bdaff7621707009e34
CodeErudition <infinity3nity360@gmail.com>
2023-10-17 16:25:27 -0700

    added the _putchar and print_str function

---
+++ func_one.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _putchar - A function that outputs character
+ *
+ * @c: The character to be printed.
+ *
+ * Return: 1 on success else -1.
+ */
+
+char _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_str - A function that prints string.
+ *
+ * @str: A pointer to the string to be printed.
+ *
+ * Return: int (in this case the number of printer characters
+ */
+
+int print_str(char *str)
+{
+	int j, cnt = 0;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+		cnt++;
+	}
+	return (cnt);
+}
