f4f4d4eeff57c37a5e79f98cdb27592847cacf85
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-18 04:21:21 +0300

    update

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 {
 	int indx = 0, char_cnt = 0;
 	va_list arg_list;
-	char spec[] = {'%', 'c', 's', 'd', 'i', 'x', 'X', 'u', 'o', 'b', 'r'};
+
 	va_start(arg_list, format);
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
@@ -25,27 +25,10 @@ int _printf(const char *format, ...)
 	{
 		while (format[indx])
 		{
-			if (format[indx] == spec[0])
+			if (format[indx] == '%')
 			{
 				indx++;
-				if (format[indx] == spec[1])
-					char_cnt += print_char(va_arg(arg_list, int));
-				else if (format[indx] == spec[2])
-					char_cnt += print_str(va_arg(arg_list, char *));
-				else if (format[indx] == spec[0])
-					char_cnt += p_print();
-				else if (format[indx] == spec[3] || format[indx] == spec[4])
-					char_cnt += print_int(va_arg(arg_list, int));
-				else if (format[indx] == spec[7])
-					char_cnt += print_unsigned(va_arg(arg_list, unsigned int));
-				else if (format[indx] == spec[5])
-					char_cnt += print_hex(va_arg(arg_list, unsigned int), 0);
-				else if (format[indx] == spec[6])
-					char_cnt += print_hex(va_arg(arg_list, unsigned int), 1);
-				else if (format[indx] == spec[8])
-					char_cnt += print_octal(va_arg(arg_list, unsigned int));
-				else if (format[indx] == spec[9])
-					char_cnt += print_binary(va_arg(arg_list, unsigned int));
+				conditions(format, arg_list, indx, char_cnt);
 			}
 			else
 				char_cnt += _putchar(format[indx]);
@@ -53,5 +36,5 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(arg_list);
-	return char_cnt;
+	return (char_cnt);
 }
